b85f3388b8831265fb84610492214205bb3abb1f
emilia98 <emilia_nedialkova@abv.bg>
2018-09-26 23:14:07 +0300

    added a solved problem

---
+++ Data Structures/Arrays/CSharp-Arrays/ArraysDS/ArraysDS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArraysDS
+{
+    class ArraysDS
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var tokens = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = ReverseArrays(tokens, n);
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+        static int[] ReverseArrays(string[] tokens, int n)
+        {
+            int len = tokens.Length;
+            int[] array = new int[n];
+
+            for (int index = 0; index < len; index++)
+            {
+                array[len - index - 1] = int.Parse(tokens[index]);
+            }
+            return array;
+        }
+    }
+}
---
+++ Data Structures/Arrays/CSharp-Arrays/ArraysDS_2/ArraysDS_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArraysDS_2
+{
+    class ArraysDS_2
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var tokens = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = ReverseArrays(tokens, n);
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+        static int[] ReverseArrays(string[] tokens, int n)
+        {
+            int len = tokens.Length;
+            int[] array = new int[n];
+            int cnt = 0;
+
+            for(int index = tokens.Length - 1; index >= 0; index--, cnt++)
+            {
+                array[cnt] = int.Parse(tokens[index]);
+            }
+
+            return array;
+        }
+    }
+}
